1de944a91e922fa82d110a77440f5cb330af57c5
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-21 22:16:26 +0330

    - Create isMaximize Filed In MainWindow.CS

--- SmartHomeUi/MainWindow.xaml.cs
+++ SmartHomeUi/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace SmartHomeUi
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isMaximize = false;
+
         public MainWindow()
         {
             InitializeComponent();
